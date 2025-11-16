3ec14ea440add03a93e91906b8f5a743d9223a8e
yadhu-vipin <yadhuvipin12@gmail.com>
2025-11-16 22:04:27 +0530

    restore point

---
+++ WinKnightUnified/WinKnightUI/RestoreTypeToBrushConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WinKnightUI
+{
+    public class RestoreTypeToBrushConverter : IValueConverter
+    {
+        private static readonly SolidColorBrush ManualBrush = new SolidColorBrush(Color.FromRgb(52, 152, 219)); // Blue
+        private static readonly SolidColorBrush AutomaticBrush = new SolidColorBrush(Color.FromRgb(231, 76, 60)); // Red
+        private static readonly SolidColorBrush DefaultBrush = new SolidColorBrush(Color.FromRgb(32, 52, 78)); // Default gray
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return DefaultBrush;
+
+            string sourceType = value.ToString().ToLower();
+
+            return sourceType switch
+            {
+                "manual" => ManualBrush,
+                "automatic" => AutomaticBrush,
+                _ => DefaultBrush
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WinKnightUnified/WinKnightUI/Services/UpdateMonitorService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace WinKnightUI.Services
+{
+    public static class UpdateMonitorService
+    {
+        private static EventLog updateLog;
+
+        public static void StartMonitoring()
+        {
+            updateLog = new EventLog("Microsoft-Windows-WindowsUpdateClient/Operational");
+            updateLog.EntryWritten += UpdateLog_EntryWritten;
+            updateLog.EnableRaisingEvents = true;
+        }
+
+        private static void UpdateLog_EntryWritten(object sender, EntryWrittenEventArgs e)
+        {
+            // EventID 19 = Successful Windows Update install
+            if (e.Entry.InstanceId == 19)
+            {
+                Task.Run(() =>
+                {
+                    try
+                    {
+                        RestoreGuardManager.CreateRestorePoint(
+                            "Automatic Update Restore Point", 12, 100
+                        );
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine("Failed automatic restore point: " + ex.Message);
+                    }
+                });
+            }
+        }
+    }
+}
